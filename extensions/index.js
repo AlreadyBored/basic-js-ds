bc2a9259bb892cc6b10e89b74053d706dbd90926
Maksim Shylau <shilaums@gmail.com>
2021-09-04 14:52:28 +0300

    init: create DS tasks repo

---
+++ extensions/index.js
@@ -0,0 +1,14 @@
+import { checkForThrowingErrors, checkForNotThrowingErrors } from './check-error.js';
+import { CONSTANTS } from './constants.js';
+import { testOptional } from './it-optional.js';
+import { NotImplementedError } from './not-implemented-error.js';
+import { ListNode } from './list-node.js';
+
+export {
+    checkForThrowingErrors,
+    checkForNotThrowingErrors,
+    CONSTANTS,
+    testOptional,
+    NotImplementedError,
+    ListNode
+};
---
+++ extensions/it-optional.js
@@ -0,0 +1,31 @@
+import { CONSTANTS } from './constants.js';
+
+const { SPECIAL_PROP_VALUE } = CONSTANTS;
+
+export function testOptional(title, fn, isAsyncTest) {
+  if (isAsyncTest) {
+    it(title, function test(done) {
+      try {
+        fn.call(this, done);
+      } catch (err) {
+        if (err._specialProp === SPECIAL_PROP_VALUE) {
+          this.test.skip();
+        } else {
+          throw err;
+        }
+      }
+    });
+  } else {
+    it(title, function test() {
+      try {
+        fn.call(this);
+      } catch (err) {
+        if (err._specialProp === SPECIAL_PROP_VALUE) {
+          this.test.skip();
+        } else {
+          throw err;
+        }
+      }
+    });
+  }
+}
