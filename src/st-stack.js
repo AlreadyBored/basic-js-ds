b3d052015613c8453b8542860484d932ba738d88
roman kostyrin <enemy-iubip@mail.ru>
2021-09-18 12:01:10 +0300

    feat: add stack task

--- src/st-stack.js
+++ src/st-stack.js
@@ -1,4 +1,4 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from '../extensions/index.js'
 
 /**
  * Implement the Stack with a given interface via array.
@@ -13,18 +13,19 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  */
 export default class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.array = []
+  }
+  push(element) {
+    this.array.push(element)
+    console.log(this.array)
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.array.pop()
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.array[this.array.length - 1]
   }
 }
