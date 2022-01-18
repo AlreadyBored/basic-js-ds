783f094a32d9903c216ea13f582d74a3c2e6538e
Maksim Shylau <shilaums@gmail.com>
2022-01-18 11:42:08 +0100

    refactor: import interface instead of definition

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,5 +1,7 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
+// const { ListNode } = require('../extensions/list-node.js');
+
 /**
  * Given a singly linked list of integers l and an integer k,
  * remove all elements from list l that have a value equal to k.
@@ -12,11 +14,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For l = [3, 1, 2, 3, 4, 5] and k = 3,
  * the output should be [1, 2, 4, 5]
  *
- * Singly - linked lists are already defined with this interface
- * function ListNode(x) {
- *   this.value = x;
- *   this.next = null;
- * }
+ * Singly - linked lists are already defined using interface ListNode from extension above
  */
 function removeKFromList(/* l, k */) {
   throw new NotImplementedError('Not implemented');
