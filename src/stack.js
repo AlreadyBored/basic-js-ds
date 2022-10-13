3367571277348c5a637610fcdc7cd261d0c538a1
AmdreiMash <100795104+AmdreiMash@users.noreply.github.com>
2022-10-13 23:56:59 +0300

    :complate stack

--- src/stack.js
+++ src/stack.js
@@ -13,22 +13,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	constructor() {
+		this.arr = []
+	}
+	push(e) {
+		return this.arr.push(e)
+	}
 
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	pop() {
+		return this.arr.pop()
+	}
 
-  peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	peek() {
+		return this.arr[this.arr.length - 1]
+	}
 }
 
 module.exports = {
-  Stack
+	Stack
 };
