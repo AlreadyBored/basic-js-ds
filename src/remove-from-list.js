d58602927c5d83cdd082e6fd2f12516218fc821c
AmdreiMash <100795104+AmdreiMash@users.noreply.github.com>
2022-10-14 01:43:59 +0300

    complate remove from list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,11 +22,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+	if (l.next !== null) {
+		if (l.value == k) {
+			l = removeKFromList(l.next, k)
+		} else {
+			l.next = removeKFromList(l.next, k)
+		}
+	} else if (l.next == null && l.value == k) {
+		l = null
+	}
+	return l
 }
 
 module.exports = {
-  removeKFromList
+	removeKFromList
 };
