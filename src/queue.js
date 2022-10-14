819e7138c178ad55e3080b5ea117cb54e94e0209
Nurlan <info.yourist@mail.ru>
2022-10-14 12:24:58 +0300

    feat:add queue solution

--- src/queue.js
+++ src/queue.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -13,21 +13,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ class Queue {
+  constructor() {
+      this.head = null;
+      this.tail = null;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+      return this.head;
+  }
+
+  enqueue(value) {
+      const lastNodeAdded = new ListNode(value);
+      if (!this.head) {
+          this.head = lastNodeAdded;
+      } else {
+          this.tail.next = lastNodeAdded;
+      }
+      this.tail = lastNodeAdded;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+      if (this.head) {
+          const { value, next } = this.head;
+          this.head = next;
+          return value;
+      }
   }
 }
 
