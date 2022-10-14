28d16e0670a49ae92bb1b2d664c1199941de0f80
Nurlan <info.yourist@mail.ru>
2022-10-14 20:11:31 +0300

    feat:add remove and stack task solution

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,5 @@
-const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+ const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,9 +21,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  if (!l) return null;
+    if (l.value === k) {
+        return removeKFromList(l.next, k);
+    } else {
+        const newNode = new ListNode(l.value);
+        newNode.next = removeKFromList(l.next, k);
+        return newNode;
+    }
 }
 
 module.exports = {
--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.value = [];
+  }
+  push(element) {
+    this.value.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.value.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.value[this.value.length - 1]
   }
 }
 
