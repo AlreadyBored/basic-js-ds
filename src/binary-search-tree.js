ce15685764bb92b048e6c0ef21aecefc7b9dcb60
KirillDemyanenko <demyanenkokg@gmail.com>
2022-10-17 14:40:46 +0300

    fix: rewrite has and find withous recursion

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -42,58 +42,91 @@ class BinarySearchTree {
   }
 
   has(data) {
-    if (data === this.rootOtTree.data) {
-        return true
-    }
-    let recursiveFind = function (link) {
-        if (link) {
+      if (this.rootOtTree) {
+          let link = this.rootOtTree;
+          while (link) {
             if (link.data === data) {
                 return true
-            } else {
-                if (link.right || link.left) {
-                    return Boolean(recursiveFind(link.left) || recursiveFind(link.right))
-                } else {
-                    return false
-                }
             }
-        }
-    }
-    if (this.rootOtTree) {
-        return Boolean(recursiveFind(this.rootOtTree.left) || recursiveFind(this.rootOtTree.right))
-    } else {
-        return false
-    }
+            if (link.data > data) {
+                link = link.left
+            } else {
+                link = link.right
+            }
+          }
+      }
+      return false
   }
+    // if (this.rootOtTree) {
+    //     if (data === this.rootOtTree.data) {
+    //         return true
+    //     }
+    //     let recursiveFind = function (link) {
+    //         if (link !== null) {
+    //             if (link.data === data) {
+    //                 return true
+    //             } else {
+    //                 if (link.right !== null || link.left !== null) {
+    //                     return Boolean(recursiveFind(link.left)) || Boolean(recursiveFind(link.right))
+    //                 } else {
+    //                     return false
+    //                 }
+    //             }
+    //         } else {
+    //             return false
+    //         }
+    //     }
+    //     return Boolean(recursiveFind(this.rootOtTree.left)) || Boolean(recursiveFind(this.rootOtTree.right))
+    // } else {
+    //     return false
+    // }
 
   find(data) {
-    let recursiveFind = function (link) {
-        if (link) {
+    if (this.rootOtTree) {
+          let link = this.rootOtTree;
+          while (link) {
             if (link.data === data) {
                 return link
-            } else {
-                if (link.right || link.left) {
-                    let resultLeft = recursiveFind(link.left)
-                    let resultRight = recursiveFind(link.right)
-                    if (resultLeft) {
-                        return resultLeft
-                    }
-                    else if (resultRight) {
-                        return resultRight
-                    } else {
-                        return null
-                    }
-                } else {
-                    return null
-                }
             }
-        }
+            if (link.data > data) {
+                link = link.left
+            } else {
+                link = link.right
+            }
+          }
     }
-    if (this.rootOtTree) {
-        return recursiveFind(this.rootOtTree)
-    } else {
-        return null
-    }
+    return null
   }
+
+  // find(data) {
+  //   let recursiveFind = function (link) {
+  //       if (link) {
+  //           if (link.data === data) {
+  //               return link
+  //           } else {
+  //               if (link.right || link.left) {
+  //                   let resultLeft = recursiveFind(link.left)
+  //                   let resultRight = recursiveFind(link.right)
+  //                   if (resultLeft) {
+  //                       return resultLeft
+  //                   }
+  //                   else if (resultRight) {
+  //                       return resultRight
+  //                   } else {
+  //                       return null
+  //                   }
+  //               } else {
+  //                   return null
+  //               }
+  //           }
+  //       }
+  //   }
+  //   if (this.rootOtTree) {
+  //       return recursiveFind(this.rootOtTree)
+  //   } else {
+  //       return null
+  //   }
+  // }
 
   // remove(data) {
   //   if (this.rootOtTree) {
@@ -146,95 +179,99 @@ class BinarySearchTree {
   remove(data) {
     let current = this.rootOtTree
     let previos = this.rootOtTree
-    if (this.rootOtTree) {
-        // удаление корня
-        if (this.rootOtTree.data === data) {
-            current = current.right;
-            while (current.left) {
-                previos = current;
-                current = current.left;
+    if (this.has(data)) {
+        if (this.rootOtTree) {
+            // удаление корня
+            if (this.rootOtTree.data === data) {
+                current = current.right;
+                while (current.left) {
+                    previos = current;
+                    current = current.left;
+                }
+                //если у самого левого элемента есть поддерево
+                if (current.right) {
+                    previos.left = current.right;
+                    current.left = this.rootOtTree.left
+                    current.right = this.rootOtTree.right
+                    this.rootOtTree = current
+                } //у самого левого элемента правого поддерева нет потомков
+                else {
+                    previos.left = null;
+                    current.left = this.rootOtTree.left
+                    current.right = this.rootOtTree.right
+                    this.rootOtTree = current;
+                }
             }
-            //если у самого левого элемента есть поддерево
-            if (current.right) {
-                previos.left = current.right;
-                current.left = this.rootOtTree.left
-                current.right = this.rootOtTree.right
-                this.rootOtTree = current
-            } //у самого левого элемента правого поддерева нет потомков
+            // не корень
             else {
-                previos.left = null;
-                current.left = this.rootOtTree.left
-                current.right = this.rootOtTree.right
-                this.rootOtTree = current;
-            }
-        }
-        // не корень
-        else {
-            while (current) {
-                if (current.data === data) {
-                    // нет потомков
-                    if (current.left === null && current.right === null) {
-                        if (current.data > previos.data) {
-                            previos.right = null
-                        } else {
-                            previos.left = null
-                        }
-                        return this
-                    }
-                    // один потомок
-                    else if (current.left === null || current.right === null) {
-                        if (current.data > previos.data) {
-                            if (current.left === null) {
-                                previos.right = current.right
+                while (current) {
+                    if (current.data === data) {
+                        // нет потомков
+                        if (current.left === null && current.right === null) {
+                            if (current.data > previos.data) {
+                                previos.right = null
                             } else {
-                                previos.right = current.left
+                                previos.left = null
                             }
-                        } else {
-                            if (current.left === null) {
-                                previos.left = current.right
-                            } else {
-                                previos.left = current.left
-                            }
+                            return this
                         }
-                        return this
-                    } // если есть оба потомка
-                    else {
-                        let elementForReplace = current.right;
-                        let elementForReplacePrevios = current
-                        while (elementForReplace.left) {
-                            elementForReplacePrevios = elementForReplace
-                            elementForReplace = elementForReplace.left
-                        }
-                        if (elementForReplace.right === null) {
-                            if (previos.data < data) {
-                                previos.right = elementForReplace;
-                                if (elementForReplace !== current.right) {
-                                    elementForReplace.right = current.right
+                        // один потомок
+                        else if (current.left === null || current.right === null) {
+                            if (current.data > previos.data) {
+                                if (current.left === null) {
+                                    previos.right = current.right
+                                } else {
+                                    previos.right = current.left
                                 }
-                                elementForReplace.left = current.left
                             } else {
-                                previos.left = elementForReplace;
+                                if (current.left === null) {
+                                    previos.left = current.right
+                                } else {
+                                    previos.left = current.left
+                                }
+                            }
+                            return this
+                        } // если есть оба потомка
+                        else {
+                            let elementForReplace = current.right;
+                            let elementForReplacePrevios = current
+                            while (elementForReplace.left) {
+                                elementForReplacePrevios = elementForReplace
+                                elementForReplace = elementForReplace.left
+                            }
+                            if (elementForReplace.right === null) {
+                                if (previos.data < data) {
+                                    previos.right = elementForReplace;
+                                    if (elementForReplace !== current.right) {
+                                        elementForReplace.right = current.right
+                                    }
+                                    elementForReplace.left = current.left
+                                } else {
+                                    previos.left = elementForReplace;
+                                    elementForReplace.right = current.right
+                                    elementForReplace.left = current.left
+                                }
+                            } else {
+                                elementForReplacePrevios.left = elementForReplace.right
+                                previos.right = elementForReplace;
                                 elementForReplace.right = current.right
                                 elementForReplace.left = current.left
                             }
-                        } else {
-                            elementForReplacePrevios.left = elementForReplace.right
-                            previos.right = elementForReplace;
-                            elementForReplace.right = current.right
-                            elementForReplace.left = current.left
+                            return this
                         }
-                        return this
+                    }
+                    else if (current.data > data) {
+                        previos = current
+                        current = current.left
+                    } else {
+                        previos = current
+                        current = current.right
                     }
                 }
-                else if (current.data > data) {
-                    previos = current
-                    current = current.left
-                } else {
-                    previos = current
-                    current = current.right
-                }
+                return this // ничего не нашлось
             }
-            return this // ничего не нашлось
+        } else {
+            return this
         }
     } else {
         return this
