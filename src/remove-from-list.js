3290bf494f378cc089f3830da0828c7ae8db3d49
Siarhei Karobka <okborok1@gmail.com>
2023-12-24 02:03:21 +0300

    feat: add linkedList remove logic

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,9 +22,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+
+  let dummy = new ListNode(-1);
+  dummy.next = l;
+  let prev = dummy;
+  let cur = l;
+
+  while (cur) {
+    if (cur.value === k) {
+      prev.next = cur.next;
+      cur = cur.next;
+    } else {
+      prev = cur;
+      cur = cur.next;
+    }
+  }
+  return dummy.next;
 }
 
 module.exports = {
