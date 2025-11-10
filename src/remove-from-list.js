adfcad71a3d0740f4af517b059341b516c43846d
RedRossum <79465904+RedRossum@users.noreply.github.com>
2025-11-10 23:51:02 +0300

    fix: fix remove-from-list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const {NotImplementedError} = require('../lib/errors');
 
-const { ListNode } = require('../extensions/list-node.js');
+const {ListNode} = require("../extensions/list-node.js");
 
 /**
  * Given a singly linked list of integers l and an integer k,
